fd94f76d4e02a6ec0e23182fe3e3b620be3c4ee5
Werick da Silva Santana <werick.s.santana34@gmail.com>
2023-05-05 18:37:55 -0300

    adding style in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 
 export function Navbar() {
+
+    let links = 'text-lg font-semibold hover:cursor-pointer hover:bg-gray-400 hover:text-gray-900 transition-colors px-2 py-1 rounded';
+
     return (
-        <header>
+        <header className="h-nav bg-gray-900 text-white flex items-center justify-between px-8 shadow-lg">
             <h1>Logo</h1>
 
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/">Explorer Next</Link></li>
-                <li><Link href="/">Contact</Link></li>
-                <li><Link href="/">About</Link></li>
+            <button className="md:hidden font-bold text-gray-200">=</button>
+            <ul className="hidden gap-x-2 md:flex">
+                <li><Link className={links} href="/">Home</Link></li>
+                <li><Link className={links} href="/">Explorer Next</Link></li>
+                <li><Link className={links} href="/">Contact</Link></li>
+                <li><Link className={links} href="/">About</Link></li>
             </ul>
         </header>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      height: {
+        'nav': '56px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
