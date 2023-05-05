39cc9f45ef1c7b29f0046249ae47f5367d39413a
Werick da Silva Santana <werick.s.santana34@gmail.com>
2023-05-05 18:43:05 -0300

    adding react-icons dependency and using icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {HiOutlineMenuAlt3} from 'react-icons/hi'
 
 export function Navbar() {
 
@@ -8,7 +9,9 @@ export function Navbar() {
         <header className="h-nav bg-gray-900 text-white flex items-center justify-between px-8 shadow-lg">
             <h1>Logo</h1>
 
-            <button className="md:hidden font-bold text-gray-200">=</button>
+            <button className="md:hidden font-bold text-gray-200">
+                <HiOutlineMenuAlt3/>
+            </button>
             <ul className="hidden gap-x-2 md:flex">
                 <li><Link className={links} href="/">Home</Link></li>
                 <li><Link className={links} href="/">Explorer Next</Link></li>
