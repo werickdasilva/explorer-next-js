4d00d4ed11b14bfc79a9a108da102db9c0752d87
Werick da Silva Santana <werick.s.santana34@gmail.com>
2023-05-05 19:03:00 -0300

    add event to navbar and close icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,31 @@
+'use client'
+
 import Link from "next/link";
 import {HiOutlineMenuAlt3} from 'react-icons/hi'
+import {MdClose} from 'react-icons/md'
+import './styled.css'
 
 export function Navbar() {
 
     let links = 'text-lg font-semibold hover:cursor-pointer hover:bg-gray-400 hover:text-gray-900 transition-colors px-2 py-1 rounded';
 
+
+    const showMenu = () => {
+        document.querySelector('#show-menu')?.classList.toggle('show')
+    }
+
     return (
         <header className="h-nav bg-gray-900 text-white flex items-center justify-between px-8 shadow-lg">
             <h1>Logo</h1>
 
-            <button className="md:hidden font-bold text-gray-200">
+            <button className="md:hidden font-bold text-gray-200" onClick={showMenu}>
                 <HiOutlineMenuAlt3/>
             </button>
-            <ul className="hidden gap-x-2 md:flex">
+            <ul id="show-menu" className="hidden gap-x-2 md:flex">
+                <button className="md:hidden font-bold text-lg text-gray-200 absolute right-2 top-2" 
+                    onClick={showMenu}>
+                    <MdClose/>
+                </button>
                 <li><Link className={links} href="/">Home</Link></li>
                 <li><Link className={links} href="/">Explorer Next</Link></li>
                 <li><Link className={links} href="/">Contact</Link></li>
